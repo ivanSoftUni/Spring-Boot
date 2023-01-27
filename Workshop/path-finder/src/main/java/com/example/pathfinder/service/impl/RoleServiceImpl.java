9b7527eb269a3b6269ec3a5f98770ef9743743d7
Ivan Kamberov <ivan_pz86@mail.bg>
2023-01-27 20:54:13 +0200

    adding services and repositories

---
+++ Workshop/path-finder/src/main/java/com/example/pathfinder/service/impl/RoleServiceImpl.java
@@ -0,0 +1,9 @@
+package com.example.pathfinder.service.impl;
+
+import com.example.pathfinder.service.RoleService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoleServiceImpl implements RoleService {
+
+}
