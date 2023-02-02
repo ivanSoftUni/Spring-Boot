6440b7b982031db450189f1fcbf35a7f903364af
Ivan Kamberov <ivan_pz86@mail.bg>
2023-02-02 08:50:42 +0200

    update html

---
+++ Workshop/path-finder/src/main/java/com/example/pathfinder/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.pathfinder.repositories;
+
+import com.example.pathfinder.models.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+}
---
+++ Workshop/path-finder/src/main/java/com/example/pathfinder/services/impl/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.pathfinder.services.impl;
+
+import com.example.pathfinder.services.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+
+}
